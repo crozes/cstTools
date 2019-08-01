8f8aa5264737f4ac820b4e1b1d92574e2a41b5af
Crozes Cyril <crozes-cyril@hotmail.fr>
2019-08-01 19:55:01 +0200

    changement pdf

--- Control/horaire/fctGenPDF.php
+++ Control/horaire/fctGenPDF.php
@@ -1,15 +1,4 @@
-<?php
-    require('../tfpdf/tfpdf.php');
-    session_start();
-    $nom = $_SESSION['Auth'][0]->nomPersonne;
-    $prenom = $_SESSION['Auth'][0]->prenomPersonne;
-
-    if(isset($_GET)){
-        $month = $_GET["month"];
-        $year = $_GET["year"];
-    }
-
-    function getFullHours(){
+function getFullHours(){
         global $month;
         global $year;
         include '../all/log_db.php';
@@ -180,40 +169,3 @@
         }
         return $newMonth;
     }
-
-    // Instanciation de la classe dérivée
-    $pdf = new PDF();
-    $pdf->AddFont('RobotoReg','','Roboto-Light.ttf',true);
-    $pdf->AddFont('Roboto-Black','','Roboto-BoldItalic.ttf',true);
-    $pdf->AddFont('RobotoTitre','','Roboto-Bold.ttf',true);
-    $pdf->AddFont('RobotoItal','','Roboto-LightItalic.ttf',true);
-    $pdf->AliasNbPages(); 
-    $pdf->AddPage();
-    $pdf->SetFont('RobotoReg','',18);
-    setlocale(LC_TIME, "fr_FR");
-    $pdf->Cell(0,0,'Déclaration Horaire de ',0,1,'C');
-    $pdf->Ln(8);
-    $pdf->SetFont('Roboto-Black','',20);
-    $pdf->Cell(0,0,getFraMonth(strftime("%B", strtotime($month."/".$month."/".$year))).' '.$year,0,1,'C');
-    $pdf->Ln(10);
-    $pdf->SetFont('RobotoReg','',12);
-    $pdf->Cell(20,0,'Nom : ',0,0);
-    $pdf->SetFont('RobotoTitre','',14);
-    $pdf->Cell(100,0,$nom,0,1);
-    $pdf->Ln(6);
-    $pdf->SetFont('RobotoReg','',12);
-    $pdf->Cell(20,0,'Prenom : ',0,0);
-    $pdf->SetFont('RobotoTitre','',14);
-    $pdf->Cell(20,0,$prenom,0,1);
-    $pdf->SetFont('RobotoReg','',10);
-    $pdf->Ln(10);
-    $header = array('Date', 'Lieu d\'intervention', 'Horaires', 'Type d\'intervention','Commentaires');
-    $pdf->FancyTable($header,getFullHours());
-    $pdf->Ln(6);
-    $pdf->SetFont('RobotoReg','',12);
-    $pdf->Cell(15,0,'Total : ',0,0);
-    $pdf->SetFont('RobotoTitre','',14);
-    $getTotalHour = getTotalHour();
-    $pdf->Cell(100,0,substr($getTotalHour['timeSum'],0,5),0,1);
-    $pdf->Output("declaHoraire".$month.$year);
-?>
