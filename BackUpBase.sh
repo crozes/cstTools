21eed53305e8cc5a2b4c7f922528029f4a4a51f0
Cyril Crozes <cyril.crozes@gmail.com>
2019-08-18 18:36:31 +0200

    Changement MAJ script

--- BackUpBase.sh
+++ BackUpBase.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd /var/www/html
-mysqldump -u root -pNanahara-31520 CST > ./CST.sql
-git add CST.sql
-git commit -m "BackUp base hebdomadaire"
-git push -u crozes -pNanahara-31520
+cd /var/www/html && mysqldump -u root -pNanahara-31520 CST > /var/www/html/CST.sql
+cd /var/www/html && git add /var/www/html/CST.sql && git commit -m "BackUp base hebdomadaire `date`"
+cd /var/www/html && git push origin master
