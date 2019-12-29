c25942bc40d1a81de43d97fc07bf903a039126c5
Cyril Crozes <cyril.crozes@gmail.com>
2019-12-29 04:00:01 +0100

    BackUp base hebdomadaire Sun 29 Dec 04:00:01 CET 2019

--- BackUpBase.sh
+++ BackUpBase.sh
@@ -2,4 +2,4 @@
 
 cd /var/www/html && mysqldump -u root -pNanahara-31520 CST > /var/www/html/CST.sql
 cd /var/www/html && git add /var/www/html/CST.sql && git commit -m "BackUp base hebdomadaire `date`"
-cd /var/www/html && git push origin
+cd /var/www/html && git push origin master
