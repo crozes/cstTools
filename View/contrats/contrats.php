8d25276caf7ff54621597834bc34b6043692c006
Crozes Cyril <crozes-cyril@hotmail.fr>
2020-04-03 21:06:15 +0200

    correction

--- View/contrats/contrats.php
+++ View/contrats/contrats.php
@@ -206,7 +206,7 @@
             $("#code_postal").val(dataReturned['codePostalPersonne']);
             $("#ville").val(dataReturned['villePersonne']);
             $("#nni").val(dataReturned['nniPersonne']);
-            $("#departement").val((dataReturned['idDepartement']==""?"NULL":dataReturned['idDepartement']));
+            $("#departement").val(dataReturned['idDepartement']);
         })
         .fail(function() {
             console.log("Error");
