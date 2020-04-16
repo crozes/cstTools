993d8782503bb79c80170d3440f4d9d6908d6979
Crozes Cyril <crozes-cyril@hotmail.fr>
2020-04-16 18:38:43 +0200

    Mdp

--- Control/account/resetPass.php
+++ Control/account/resetPass.php
@@ -1,7 +1,7 @@
 <?php
     function association($uuidRezMdp,$mailRezMdp){
         global $PDO;
-        $sql_select = 'INSERT INTO RezMdp (uuidRezMdp,mailRezMdp,dateRezMdp) VALUES ('.$uuidRezMdp.','.$mailRezMdp.','.date('Y\/m\/d').');';
+        $sql_select = 'INSERT INTO RezMdp (idRezMdp,uuidRezMdp,mailRezMdp,dateRezMdp) VALUES (NULL,'.$uuidRezMdp.','.$mailRezMdp.','.date('Y-m-d').');';
         $req = $PDO->prepare($sql_select);
         $req->execute();
     }
